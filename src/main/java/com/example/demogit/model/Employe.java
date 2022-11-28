7427b6491bb94acb83da38f454a2a679e3612b12
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-11-28 16:10:33 +0100

    first commit bis

---
+++ src/main/java/com/example/demogit/model/Employe.java
@@ -0,0 +1,57 @@
+package com.example.demogit.model;
+
+public class Employe {
+    private long id;
+    private String prenom;
+    private String nom;
+    private String email;
+
+    public Employe(long id, String prenom, String nom, String email) {
+        this.id = id;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "id=" + id +
+                ", prenom='" + prenom + '\'' +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
